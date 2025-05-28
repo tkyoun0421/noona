4cfd2517a61cf153fd053bd09512ec4b2ba98493
Taegwan Youn <tkyoun0421@naver.com>
2025-05-28 17:16:17 +0900

    day03: quest-6 테스트 코드 완성

---
+++ week-01/src/day-03/test/quest-6.test.ts
@@ -0,0 +1,109 @@
+import { describe, expect, it } from "vitest";
+
+const context = describe;
+describe("문제 1. 다양한 데이터 타입을 입력받아, 입력에 따라 다른 처리를 수행하는 함수를 작성하세요", () => {
+  it("숫자 배열을 인자로 받으면 배열 아이템들의 합계를 반환한다", () => {
+    const numberArray = [1, 2, 3];
+
+    const result = processInput(numberArray);
+
+    expect(result).toBe(6);
+  });
+  it("문자열 배열을 인자로 받으면 배열의 아이템들의 문자열을 연결하여 반환한다", () => {
+    const stringArray = ["hello", "world"];
+
+    const result = processInput(stringArray);
+
+    expect(result).toBe("helloworld");
+  });
+  it("객체를 인자로 받으면 객체의 message 속성을 대문자로 변환하여 반환한다", () => {
+    const object = { message: "TypeScript" };
+
+    const result = processInput(object);
+
+    expect(result).toBe("TYPESCRIPT");
+  });
+
+  it("숫자 문자열 배열 또는 객체를 제외한 다른 값을 받으면 에러를 반환한다", () => {
+    const foo = 42;
+
+    const result = processInput(foo);
+
+    expect(result).toThrowError();
+  });
+});
+
+describe("문제 2. 다음 조건을 만족하는 코드를 작성하세요", () => {
+  context("1. 아래와 같은 두 개의 클래스를 정의합니다", () => {
+    it("Car 클래스: brand(브랜드 이름, 문자열) 속성을 가집니다", () => {
+      expect(car).toHaveProperty("brand");
+      expect(typeof car.brand).toBe("string");
+      expect(car.brand).toBe("Hyundai");
+    });
+
+    it("Bike 클래스: type(바이크 종류, 문자열) 속성을 가집니다", () => {
+      expect(bike).toHaveProperty("type");
+      expect(typeof bike.type).toBe("string");
+      expect(bike.type).toBe("Scott");
+    });
+  });
+
+  context("2. 입력값이 Car 또는 Bike의 인스턴스일 수 있는 vehicle을 받아 다음 규칙에 따라 처리하는 함수를 작성하세요", () => {
+    it("Car이면 브랜드 이름을 대문자로 반환합니다", () => {
+      const car = new Car("benz");
+
+      const result = processVehicle(car);
+
+      expect(result).toBe("BENZ");
+    });
+    it("Bike이면 바이크 종류 앞에 Bike: 를 추가하여 반환합니다", () => {
+      const bike = new Bike("Scott");
+
+      const result = processVehicle(bike);
+
+      expect(result).toBe("Bike: Scott");
+    });
+  });
+});
+
+describe("문제 3. in을 활용한 사용자 관리", () => {
+  it("Admin: 권한 목록(permissions)을 ,로 연결한 문자열을 반환합니다", () => {
+    const adminUser = { type: "admin", permissions: ["read", "write"] };
+
+    const result = processUser(adminUser);
+
+    expect(result).toBe("read,write");
+  });
+  it("User: 이메일 주소(email)을 반환합니다", () => {
+    const user = { type: "user", email: "user@example.com" };
+
+    const result = processUser(user);
+
+    expect(result).toBe(user.email);
+  });
+});
+
+describe("문제 4. 아래와 같은 유니온 타입을 처리하는 함수를 작성하세요", () => {
+  it("Rectangle이면 넓이를 반환합니다. (가로 × 세로)", () => {
+    const rectangle = { width: 10, height: 5 };
+
+    const result = calculateArea(rectangle);
+
+    expect(result).toBe(50);
+  });
+  it("Circle이면 넓이를 반환합니다. (π × 반지름²)", () => {
+    const circle = { radius: 7 };
+
+    const result = calculateArea(circle);
+
+    expect(result).toBe(153.93804002589985);
+  });
+});
+
+describe("문제 5. 유니온 타입의 문제점과 해결 방법", () => {
+  it("assertExhaustive 함수를 추가하여 새로운 타입이 추가되었는데 처리를 하지 않으면 에러를 반환한다", () => {
+    const square = { size: 5 };
+
+    expect(() => calculateArea(square)).toThrowError();
+  });
+});
